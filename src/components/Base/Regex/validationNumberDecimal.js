34e4c0e3a1b636d3741315ece0e4d664709cab90
quandx <quandx.bkav@gmail.com>
2023-04-03 08:37:33 +0700

    Đẩy regex soos thaapj phaan

---
+++ src/components/Base/Regex/validationNumberDecimal.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2021-present, Bkav, Cop.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Bkav license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @author quandx.bkav@gmail.com on 01/04/2023.
+ *
+ * History:
+ * @modifier abc@bkav.com on xx/xx/xxxx đã chỉnh sửa abcxyx (Chỉ các thay đổi quan trọng mới cần ghi lại note này)
+ */
+
+const isCheckNumberDecimal = (number) => {
+	if (typeof number !== 'number') {
+		return false;
+	}
+	return /^\d+(\.\d+)?$/.test(number) && Number(number) > 0;
+};
+
+export { isCheckNumberDecimal };
